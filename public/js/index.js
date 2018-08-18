fcfd52163f1de8ffb635cd5a224d05798d4df81e
agdhruv <dhruv_agarwal@live.com>
2018-08-18 08:52:34 +0530

    Start script

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,17 @@
 var socket = io();
 
+var sayMessage = function (message) {
+
+	if ('speechSynthesis' in window) {
+		var msg = new SpeechSynthesisUtterance(message);
+		var voices = window.speechSynthesis.getVoices();
+		msg.voice = voices[9];
+		msg.rate = 7;
+		window.speechSynthesis.speak(msg);
+	}
+
+}
+
 socket.on('connect', function () {
 	console.log('Connected to server');
 
@@ -34,6 +46,7 @@ socket.on('accident', (data) => {
 			url: URL,
 			crossDomain: true,
 			success: function (data) {
+				sayMessage('There has been an accident 10 minutes away from you. Please slow down and drive carefully.');
 				console.log(data);
 			},
 			dataType: 'json'
